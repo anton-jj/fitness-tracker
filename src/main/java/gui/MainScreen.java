f1305ab639eab604cde85ab6fa05e0e2b575d0b8
anton <jappinen.anton@protonmail.com>
2025-06-16 18:41:46 +0200

    just testing some gui components

--- src/main/java/gui/MainScreen.java
+++ src/main/java/gui/MainScreen.java
@@ -1,22 +1,38 @@
 package gui;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MainScreen {
 
 	public void start(Stage primaryStage) {
 		BorderPane root = new BorderPane();
-        Label welcomeLabel = new Label("welcome to the fitness-tracker");
-        root.setCenter(welcomeLabel);
-
-        Scene scene = new Scene(root, 500, 800);
-
-        primaryStage.setTitle("fitness-tracker");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+		root.setTop(setupButtons());
+		Scene scene = new Scene(root,600,500, Color.BEIGE);
+		
+		
+		primaryStage.setTitle("fitness-tracker");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 
 	}
+	
+	private HBox setupButtons() {
+		HBox buttonBar = new HBox(10);
+		
+		Button createProgram = new Button("create program");
+		Button logWeight = new Button("log weight");
+		Button logWorkout = new Button("log workout");
+		
+		buttonBar.getChildren().addAll(createProgram, logWeight, logWorkout);
+		return buttonBar;
+	}
+	
+	
 }
